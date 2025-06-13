707e65f7c35ea91fcd9c15756d0461c74d4de2f6
frontenddev <frontendev344@gmail.com>
2025-06-13 17:22:22 +0530

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,4 +33,37 @@ var swiper = new Swiper(".swiper", {
   }
 });
 
+
+  // Read More Functionality JS Start
+  $('.subpage-readmore-btn').click(function (e) {
+    e.preventDefault();
+    const $button = $(this);
+    const $content = $button.closest('.destination-card-text').find('.destination-para');
+
+    $content.stop(true, true).slideToggle(500, function () {
+        const isVisible = $content.is(':visible');
+
+        $button.html(isVisible
+            ? 'Read Less <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>' // UP arrow
+            : 'Read More <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>' // DOWN arrow
+        );
+    });
+});
+
+  // Read More Functionality JS end
+
+
+  
+// Read More Functionality JS Start
+
+// $('.subpage-readmore-btn').click(function (e) {
+//   e.preventDefault();
+//   // console.log('click');
+//   $(this).parent().prev().slideToggle('slow');
+//   // $('.subpage-exp-content').slideToggle('slow');
+//   $(this).text($(this).text() == 'Read less' ? 'Read on' : 'Read less');
+// });
+
+// Read More Functionality JS end
+
   
