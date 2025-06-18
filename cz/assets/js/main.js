ce6c77ee9e02a90ad9f169e9aea44bf3cc44fe96
frontenddev <frontendev344@gmail.com>
2025-06-18 18:10:48 +0530

    change

--- cz/assets/js/main.js
+++ cz/assets/js/main.js
@@ -1,21 +1,21 @@
-  // mobil
+// mobil
 document.addEventListener("DOMContentLoaded", function () {
   const searchIcon = document.getElementById("search-icon");
   const nav = document.querySelector(".nav");
   const menuToggle = document.querySelector(".menu-toggle");
 
   if (searchIcon) {
-      searchIcon.addEventListener("click", function () {
-          nav.classList.toggle("search");
-          nav.classList.toggle("no-search");
-      });
+    searchIcon.addEventListener("click", function () {
+      nav.classList.toggle("search");
+      nav.classList.toggle("no-search");
+    });
   }
 
   if (menuToggle) {
-      menuToggle.addEventListener("click", function () {
-          nav.classList.toggle("mobile-nav");
-          this.classList.toggle("is-active");
-      });
+    menuToggle.addEventListener("click", function () {
+      nav.classList.toggle("mobile-nav");
+      this.classList.toggle("is-active");
+    });
   }
 });
 
@@ -34,26 +34,26 @@ var swiper = new Swiper(".swiper", {
 });
 
 
-  // Read More Functionality JS Start
-  $('.subpage-readmore-btn').click(function (e) {
-    e.preventDefault();
-    const $button = $(this);
-    const $content = $button.closest('.destination-card-text').find('.destination-para');
+// Read More Functionality JS Start
+$('.subpage-readmore-btn').click(function (e) {
+  e.preventDefault();
+  const $button = $(this);
+  const $content = $button.closest('.destination-card-text').find('.destination-para');
 
-    $content.stop(true, true).slideToggle(500, function () {
-        const isVisible = $content.is(':visible');
+  $content.stop(true, true).slideToggle(500, function () {
+    const isVisible = $content.is(':visible');
 
-        $button.html(isVisible
-            ? 'Read Less <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>' // UP arrow
-            : 'Read More <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>' // DOWN arrow
-        );
-    });
+    $button.html(isVisible
+      ? 'Read Less <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>' // UP arrow
+      : 'Read More <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>' // DOWN arrow
+    );
+  });
 });
 
-  // Read More Functionality JS end
+// Read More Functionality JS end
 
 
-  
+
 // Read More Functionality JS Start
 
 // $('.subpage-readmore-btn').click(function (e) {
@@ -66,4 +66,31 @@ var swiper = new Swiper(".swiper", {
 
 // Read More Functionality JS end
 
-  
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdownBtn = document.getElementById('languageDropdownBtn');
+  const dropdownMenu = document.querySelector('#langDropdown .dropdown-menu');
+
+  dropdownBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    dropdownMenu.classList.toggle('show');
+  });
+
+  document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function (event) {
+      event.preventDefault();
+      const href = this.getAttribute('href');
+      window.location.href = href;
+    });
+  });
+
+  document.addEventListener('click', function (event) {
+    if (!document.getElementById('langDropdown').contains(event.target)) {
+      dropdownMenu.classList.remove('show');
+    }
+  });
+});
+
+
